367b4b95a4bbe9d87527fa5863717334720ea467
Michael <50074867+brndnwclnst@users.noreply.github.com>
2021-10-05 23:12:39 +0100

    consts workaround

--- js/consts.js
+++ js/consts.js
@@ -1,1 +1,7 @@
-const webroot = "http://127.0.0.1:8000";
+var webroot = "";
+
+if (window.location == "https://michaelodonnell13.github.io/") {
+    webroot = "https://www.ochenos.com";
+} else {
+    webroot = "http://127.0.0.1:8000";
+}
