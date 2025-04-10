6cb55c6ca1342588253275207d3bdf45db8e0dc9
Michael <50074867+michaelodonnell13@users.noreply.github.com>
2025-04-10 08:09:49 +0100

    Update to reflect new api. remove old things. Tidy up.

--- js/consts.js
+++ js/consts.js
@@ -1,7 +1,7 @@
 var webroot = "";
 
 if (window.location.origin == "https://michaelodonnell13.github.io") {
-    webroot = "https://www.ochenos.com";
+    webroot = "https://cv2.ochenos.com";
 } else {
     webroot = "http://127.0.0.1:8000";
 }
